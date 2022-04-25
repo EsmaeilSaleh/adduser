72cf9682812e9f020de2754f355ab24f90196b0f
e.saleh88@gmail.com <e.saleh88@gmail.com>
2022-04-25 22:07:47 +0430

    make boilerplate

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AddUser = () => {
+  return (
+    <div>
+      <h2>AddUser</h2>
+      <form>
+        <label htmlFor="username">Username: </label>
+        <input type="text" id="username" />
+      </form>
+    </div>
+  );
+};
+
+export default AddUser;
