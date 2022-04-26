4c1819f3f43f1424c81430a0b806bc472c80787f
e.saleh88@gmail.com <e.saleh88@gmail.com>
2022-04-27 00:28:47 +0430

    remove scroll bar

--- src/components/UI/Footer.js
+++ src/components/UI/Footer.js
@@ -7,7 +7,7 @@ import Typography from "@mui/material/Typography";
 const Footer = () => {
   return (
     <Box sx={{ position: "absolute", bottom: 0 }}>
-      <Divider sx={{ width: "100vw", border: "1px solid grey" }}></Divider>
+      <Divider sx={{ width: "98vw", border: "1px solid grey" }}></Divider>
       <Typography
         sx={{ textAlign: "center", padding: "0.5em", color: "#8f8f8f" }}
       >
