3158c85fec2345f700d8ee6ed60842c5d2133f62
e.saleh88@gmail.com <e.saleh88@gmail.com>
2022-04-27 00:27:04 +0430

    change style

--- src/components/UI/Footer.js
+++ src/components/UI/Footer.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
 
 const Footer = () => {
-        return (
-                <div>
-                        <h2>Footer</h2>
-                </div>
-                )
-}
+  return (
+    <Box sx={{ position: "absolute", bottom: 0 }}>
+      <Divider sx={{ width: "100vw", border: "1px solid grey" }}></Divider>
+      <Typography
+        sx={{ textAlign: "center", padding: "0.5em", color: "#8f8f8f" }}
+      >
+        Coded Proudly by Esmaeil Saleh
+      </Typography>
+    </Box>
+  );
+};
 
-export default Footer
+export default Footer;
