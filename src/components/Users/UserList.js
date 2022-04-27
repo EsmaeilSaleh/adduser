6d3dc3e30f0c01973a05e1ac7810426b2cf90cb9
e.saleh88@gmail.com <e.saleh88@gmail.com>
2022-04-27 22:07:46 +0430

    userlist update

--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -2,15 +2,18 @@ import React from "react";
 
 import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
 
 const UserList = (props) => {
   return (
     <Card sx={{ marginTop: "0.8em" }}>
-      <Typography variant="h2">UserList</Typography>
+      <Typography variant="h2" sx={{ textAlign: 'center' }}>UserList</Typography>
       {props.users.map((user) => (
-        <li>
-          {user.name} ({user.age})
-        </li>
+        <Box sx={{ padding: '.8em', textAlign: 'center' }}>
+            {user.name} ({user.age})
+          <Divider sx={{ marginTop: '0.8em' }}/>
+        </Box>
       ))}
     </Card>
   );
